cead1c8443c637d4264b208fdf8beea54a8be81d
hr1sh1kesh <hr1sh1kesh.deodhar@outlook.com>
2019-12-23 19:37:46 +0530

    Activity worker

--- helloworld_activity.go
+++ helloworld_activity.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 
+	"github.com/hr1sh1kesh/hwworkflow/wf"
 	"go.uber.org/cadence/activity"
+	"go.uber.org/cadence/workflow"
 )
 
 func init() {
+	workflow.Register(wf.HelloWorldWorkflow)
 	activity.Register(HelloworldActivity)
-
 }
 
 func HelloworldActivity(ctx context.Context, name string) (string, error) {
